1a4da1956705128be6129c0668e94e998a50c1e2
guinegrijo <guilhermenegrijo@gmail.com>
2025-04-14 16:15:39 -0300

    Implementação de EventosScreen

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { createStackNavigator } from "@react-navigation/stack"
 import Layout from "./components/Layout";
 import TaskList from "./screens/TaskList";
 import TaskDetail from "./screens/TaskDetail";
+import EventoScreen from "./screens/EventosScreen";
 
 const Stack = createStackNavigator();
 
@@ -16,23 +17,30 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="TaskList">
-        <Stack.Screen name="TaskList" component={TaskList}>
+        <Stack.Screen name="EventoScreen"component={()=>(
+          <Layout>
+            <EventoScreen/>
+          </Layout>
+        )}/>
 
+        {/* <Stack.Screen name="TaskList" component={TaskList}>
         </Stack.Screen>
+
         <Stack.Screen name="TaskDetail" component={TaskDetail}>
-
-        </Stack.Screen>
+        </Stack.Screen> */}
 
         <Stack.Screen name="Login" component={()=>(
           <Layout>
             <LoginScreen/>
           </Layout>
         )}/>
+
         <Stack.Screen name="Cadastro" component={()=>(
           <Layout>
             <CadastroScreen/>
           </Layout>
         )}/>
+
         <Stack.Screen name="Home" component={HomeScreen}/>
         <Stack.Screen name="Evento" component={EventoScreen}/>
         <Stack.Screen name="Organizador" component={OrganizadorScreen}/>
--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://10.89.240.71:5000/api/v1/",
+    baseURL: "http://localhost:5000/api/v1/",
     headers: {"accept": "application/json"}
 })
 
@@ -10,7 +10,8 @@ const sheets =  {
     postCadastro: (user) => api.post("user", user),
     postEvento: (evento) => api.post("evento", evento),
     postCadastroOrganizador: (organizador) => api.post("organizador", organizador),
-    postCadastroIngresso: (ingresso) => api.post('ingresso', ingresso)
+    postCadastroIngresso: (ingresso) => api.post('ingresso', ingresso),
+    getEventos: () => api.get("evento")
 }
 
 export default sheets
